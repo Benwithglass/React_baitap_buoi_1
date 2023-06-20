92da74deb37ebd43794d4c1880add0ce35861935
Pham Tien Dat <tiendatpham2412@gmail.com>
2023-06-20 21:06:54 +0700

    chinh mau & layout

--- src/BaitapLayOutComponent/Content.js
+++ src/BaitapLayOutComponent/Content.js
@@ -5,9 +5,32 @@ export default class extends Component {
   render() {
     return (
       <div class="container mt-4 mb-4 d-flex justify-content-between">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png" className="card-img-top" alt="..." />
-          <div className="card-body text-center">
+        <div className="card" style={{ width: "18rem", backgroundColor:"#F8F9FA"}}>
+          <img
+            src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body text-center border-top bg-warning">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <div class="button">
+              <a href="#" className="btn btn-primary">
+                Find out more!
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem", backgroundColor:"#F8F9FA"}}>
+          <img
+            src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body text-center border-top bg-warning">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
@@ -18,9 +41,13 @@ export default class extends Component {
             </a>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png" className="card-img-top" alt="..." />
-          <div className="card-body text-center">
+        <div className="card" style={{ width: "18rem", backgroundColor:"#F8F9FA"}}>
+          <img
+            src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body text-center border-top bg-warning">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
@@ -31,22 +58,13 @@ export default class extends Component {
             </a>
           </div>
         </div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png" className="card-img-top" alt="..." />
-          <div className="card-body text-center">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Find out more!
-            </a>
-          </div>
-        </div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png" className="card-img-top" alt="..." />
-          <div className="card-body text-center">
+        <div className="card" style={{ width: "18rem", backgroundColor:"#F8F9FA"}}>
+          <img
+            src="https://www.pngmart.com/files/3/Ice-Cream-Scoop-PNG-Picture.png"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body text-center border-top bg-warning">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
